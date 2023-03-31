d9b7e6f19150925f953fd976ed570192fa204d22
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-03-31 11:37:43 +0400

    created simple text to speak functionality with SpeechSynthesis

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
+  const synth = useRef<SpeechSynthesis | null>(null);
+
+  useEffect(() => {
+    if (window.speechSynthesis) {
+      synth.current = window.speechSynthesis;
+    }
+  }, []);
+
+  const speak = () => {
+    synth.current?.speak(new SpeechSynthesisUtterance("hello"));
+  };
+
   return (
     <div className="App">
       <div>
@@ -12,7 +24,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button>Buton</button>
+        <button onClick={speak}>Speak</button>
       </div>
     </div>
   );
