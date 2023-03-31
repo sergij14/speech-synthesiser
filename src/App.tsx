73cb3d8be5c0c364a398e5f7351bc4acc7e1e86e
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2023-03-31 11:55:37 +0400

    rendering available voices options

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,39 @@ import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+const VoiceSelect = ({ voices }: { voices?: SpeechSynthesisVoice[] }) => (
+  <select>
+    {(voices || []).map((voice) => (
+      <option key={voice.voiceURI} value={voice.name}>
+        {voice.name}
+      </option>
+    ))}
+  </select>
+);
+
 function App() {
   const synth = useRef<SpeechSynthesis | null>(null);
   const [text, setText] = useState("");
+  const [availableVoices, setAvailableVoices] =
+    useState<SpeechSynthesisVoice[]>();
 
   useEffect(() => {
     if (window.speechSynthesis) {
       synth.current = window.speechSynthesis;
+      setAvailableVoices(synth.current.getVoices());
     }
   }, []);
+
+  useEffect(() => {
+    if (synth.current && !availableVoices?.length) {
+      setAvailableVoices(synth.current.getVoices());
+    }
+  }, [availableVoices]);
 
   const speak = (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
     synth.current?.speak(new SpeechSynthesisUtterance(text));
-    setText('')
+    setText("");
   };
 
   return (
@@ -28,9 +47,16 @@ function App() {
       <h1>Speech Synthesieser</h1>
       <div className="card">
         <form onSubmit={speak}>
-          <input value={text} type="text" onChange={({ target }) => setText(target.value)} />
-          <button disabled={!text.length} type="submit">Speak</button>
+          <input
+            value={text}
+            type="text"
+            onChange={({ target }) => setText(target.value)}
+          />
+          <button disabled={!text.length} type="submit">
+            Speak
+          </button>
         </form>
+        <VoiceSelect voices={availableVoices} />
       </div>
     </div>
   );
